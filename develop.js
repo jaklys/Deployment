0747a400b6ab3a6ac5a7a417f51512b14eb2e6a8
jaklys <machacek.jan2@gmail.com>
2024-12-04 15:39:44 +0100

    t

--- develop.js
+++ develop.js
@@ -6,3 +6,19 @@
  [data-theme="dark"] tr.has-background-primary:hover {
     background-color: #2b4764 !important;
  }
+
+
+
+ 
+
+ [data-theme="dark"] .table tr.has-background-primary {
+    background-color: #375a7f !important;
+ }
+ 
+ [data-theme="dark"] .table tr.has-background-primary:hover {
+    background-color: #2b4764 !important;
+ }
+ 
+ [data-theme="dark"] .table tr.has-background-primary td {
+    color: #fff !important;
+ }
